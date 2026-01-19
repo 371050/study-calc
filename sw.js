381c7f9f1b302240bb390f48c9a10ea816b824cb
SOMA NISHIMURA <n.soma3711@gmail.com>
2026-01-19 13:18:19 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE_NAME = "calc-pwa-cache-v1";
+const CACHE_NAME = "study-pwa-cache-v2";
 const ASSETS = [
   "./",
   "./index.html",
@@ -14,11 +14,13 @@ const ASSETS = [
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
+
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME ? caches.delete(k) : null)))
   );
 });
+
 self.addEventListener("fetch", (event) => {
   if (event.request.method !== "GET") return;
   event.respondWith(
